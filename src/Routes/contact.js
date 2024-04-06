ddc6f5a1c19ad23134df0989988419d5e2494617
laxman1892 <lxmnrzl@gmail.com>
2024-04-06 19:11:27 +0545

    contact form without css

--- src/Routes/contact.js
+++ src/Routes/contact.js
@@ -2,7 +2,18 @@ import React from 'react'
 
 function Contact() {
   return (
-    <div>Contact</div>
+    <div className="contact">
+        <h3>Contact Me</h3>
+        <form action="" method="post">
+            <label htmlFor="name">Name: </label>
+            <input type="text" name="name" id="name"/><br/>
+            <label htmlFor="email">Email: </label>
+            <input type="email" name="email" id="email"/><br/>
+            <label htmlFor="message">Message: </label>
+            <textarea name="message" id="message"></textarea><br/>
+            <button type="submit">Submit</button>
+        </form>
+    </div>
   )
 }
 
