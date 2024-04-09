6e45b0ca7bb29e3e744622b595a4f51e731bfc51
laxman1892 <lxmnrzl@gmail.com>
2024-04-09 20:42:32 +0545

    update component content

--- src/aboutme.js
+++ src/aboutme.js
@@ -1,7 +1,17 @@
 import React from 'react'
 function Aboutme() {
   return (
-    <div>About Me</div>
+    <div style={{display: 'flex', alignItems: 'center', height: 'inherit'}}>
+      <div className='hero-des'>
+        <h1 className='small-heading'>Hi, My name is</h1>
+      <h1 className='big-heading'>Laxman Rijal.</h1>
+      <h1 className='big-heading'>I build things for the web.</h1>
+      <div className="hero-section">
+        <h1 className='small-heading' style={{marginTop: 10}}>I’m a software engineer specializing in building and designing beautiful web experiences.</h1>
+      <div style={{width: '70%'}}></div>
+      </div>
+    </div>
+    </div>
   )
 }
 export default Aboutme
