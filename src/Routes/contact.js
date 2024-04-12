5ed8c1a79c3cf770781ed1eb79d943c11b983071
laxman1892 <lxmnrzl@gmail.com>
2024-04-12 20:48:54 +0545

    email validation and a lot of CSS shit

--- src/Routes/contact.js
+++ src/Routes/contact.js
@@ -1,7 +1,96 @@
-import React from "react";
+import React, { useState } from "react";
 import "../App.css";
 
 function Contact() {
+  const [nameValid, setNameValid] = useState(true);
+  const [emailValid, setEmailValid] = useState(true);
+  const [nameError, setNameError] = useState("");
+  const [emailError, setEmailError] = useState("");
+  const [nameFocused, setNameFocused] = useState(false);
+  const [emailFocused, setEmailFocused] = useState(false);
+  const [messageFocused, setMessageFocused] = useState(false);
+
+  const handleInputChange = (event) => {
+    const { name, value } = event.target;
+    let isValid = true;
+    let errorMessage = "";
+
+    switch (name) {
+      case "name":
+        isValid = value.match(/^[a-zA-Z\s]+$/);
+        errorMessage = isValid ? "" : "Please enter a valid name.";
+        break;
+      case "email":
+        // eslint-disable-next-line
+        isValid = value.match(/^[a-z0-9._%+\-]+@[a-z0-9.\-]+\.[a-z]{2,}$/);
+        errorMessage = isValid ? "" : "Please enter a valid email address.";
+        break;
+      default:
+        break;
+    }
+
+    switch (name) {
+      case "name":
+        setNameValid(isValid);
+        setNameError(errorMessage);
+        setNameFocused(false);
+        break;
+      case "email":
+        setEmailValid(isValid);
+        setEmailError(errorMessage);
+        setEmailFocused(false);
+        break;
+      case "message":
+        setMessageFocused(false);
+        break;
+      default:
+        break;
+    }
+  };
+
+  const handleFocus = (event) => {
+    const { name } = event.target;
+    switch (name) {
+      case "name":
+        setNameValid(true);
+        setNameFocused(true);
+        break;
+      case "email":
+        setEmailValid(true);
+        setEmailFocused(true);
+        break;
+      case "message":
+        setMessageFocused(true);
+        break;
+      default:
+        break;
+    }
+  };
+
+  const getInputClass = (name) => {
+    switch (name) {
+      case "name":
+        return nameValid ? " " : "invalid";
+      case "email":
+        return emailValid ? " " : "invalid";
+      default:
+        return "";
+    }
+  };
+
+  const Input = document.querySelectorAll(".form-input");
+
+  Input.forEach((input) => {
+    input.addEventListener("focus", function () {
+      let parent = this.parentNode;
+      parent.classList.add("focus");
+    });
+    input.addEventListener("blur", function () {
+      let parent = this.parentNode;
+      parent.classList.remove("focus");
+    });
+  });
+
   return (
     <div className="contact">
       <div className="field-left">
@@ -14,8 +103,17 @@ function Contact() {
         </div>
       </div>
       <div className="field-right">
-        <form className='gform' action="https://script.google.com/macros/s/AKfycbzVcr034xIE79ENPoXCVZlwdlYTvvOIQ4Xy-Iloo-6RFXMbPLGU500lgIINVfXQZrsY/exec" method="post">
-          <div className="msg">
+        <form
+          className="gform"
+          action="https://script.google.com/macros/s/AKfycbzVcr034xIE79ENPoXCVZlwdlYTvvOIQ4Xy-Iloo-6RFXMbPLGU500lgIINVfXQZrsY/exec"
+          method="post"
+          onSubmit={(event) => event.preventDefault()}
+        >
+          <div
+            className={`msg ${getInputClass("name")} ${
+              nameFocused ? "focus" : ""
+            }`}
+          >
             <label htmlFor="name" className="form-label">
               Name
             </label>
@@ -25,11 +123,19 @@ function Contact() {
               type="text"
               name="name"
               id="name"
-              className="form-input"
+              className={`form-input ${getInputClass("name")} `}
+              onBlur={handleInputChange}
+              onFocus={handleFocus}
+              autoComplete="off"
             />
             <br />
           </div>
-          <div className="msg">
+          {!nameValid && <p className="error">{nameError}</p>}
+          <div
+            className={`msg  ${getInputClass("email")} ${
+              emailFocused ? "focus" : ""
+            }`}
+          >
             <label htmlFor="email" className="form-label">
               Email
             </label>
@@ -39,11 +145,15 @@ function Contact() {
               type="email"
               name="email"
               id="email"
-              className="form-input"
+              className={`form-input ${getInputClass("email")}`}
+              onBlur={handleInputChange}
+              onFocus={handleFocus}
+              autoComplete="off"
             />
             <br />
           </div>
-          <div className="msg">
+          {!emailValid && <p className="error">{emailError}</p>}
+          <div className={`msg ${messageFocused ? "focus" : ""}`}>
             <label htmlFor="message" className="form-label">
               Message
             </label>
@@ -56,7 +166,9 @@ function Contact() {
             ></textarea>
             <br />
           </div>
-          <button type="submit" className="message-button">Send Message</button>
+          <button type="submit" className="message-button">
+            Send Message
+          </button>
         </form>
       </div>
     </div>
